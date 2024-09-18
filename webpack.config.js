d194920a348e54f1f33a39584ef97323f9e3a220
Andrey Sunsin <andreysunone@gmail.com>
2024-09-18 14:50:10 +0500

    png to webp in styles

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { ProvidePlugin } = require('webpack');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 const environment = require('./configuration/environment');
 
 function ppath(p) {
@@ -64,13 +64,8 @@ module.exports = {
       {
         test: /\.(png|gif|jpe?g|svg)$/i,
         type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: environment.limits.images,
-          },
-        },
         generator: {
-          filename: 'images/[name].[hash:6][ext]',
+          filename: 'images/[name].[ext]',
         },
       },
       {
@@ -102,45 +97,11 @@ module.exports = {
     //     },
     //   },
     // },
-    minimizer: [
-      '...',
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                'svgo',
-                {
-                  plugins: [
-                    {
-                      name: 'removeViewBox',
-                      active: false,
-                    },
-                  ],
-                },
-              ],
-            ],
-          },
-        },
-      }),
-    ],
   },
   resolve: {
     alias: {
-      // jquery is NOT a peer dependency in jquery.inputmask so a alias
-      // is used here to force jquery.inputmask to use your jquery
-      // version
       jquery: ppath('node_modules/jquery/dist/jquery'),
-      // Switch dependency lib accordingly (this one uses jquery)
       'inputmask.dependencyLib': ppath('node_modules/jquery.inputmask/dist/inputmask/inputmask.dependencyLib'),
-      // Core library (order of these aliases shouldn't matter FYI)
       inputmask: ppath('node_modules/jquery.inputmask/dist/inputmask/inputmask'),
       'jquery.inputmask': ppath('node_modules/jquery.inputmask/dist/inputmask/jquery.inputmask'),
       'inputmask.numeric.extensions': ppath('node_modules/jquery.inputmask/dist/inputmask/inputmask.numeric.extensions'),
@@ -148,6 +109,18 @@ module.exports = {
     },
   },
   plugins: [
+    new ImageminWebpWebpackPlugin({
+      config: [{
+        test: /\.(png|gif|jpe?g)$/i,
+        options: {
+          quality: 90,
+        },
+      }],
+      overrideExtension: true,
+      detailedLogs: false,
+      silent: false,
+      strict: true,
+    }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
     }),
