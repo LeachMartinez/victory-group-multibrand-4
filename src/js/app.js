82dabd1adc470854c5317946e85699c823d5dfe0
valentin.isaev2701 <valentin.isaev2701@yandex.ru>
2024-11-12 15:53:36 +0300

    Добавил пиксели до предзагрузки

--- src/js/app.js
+++ src/js/app.js
@@ -286,6 +286,7 @@ window.app = {
   runLazy: () => {
     $('.lazy').Lazy({
       // visibleOnly: true,
+      threshold: 800, // изображения начнут загружаться за 500 пикселей до того, как они окажутся видимыми
       combined: true,
       afterLoad: function(element) {
         element.addClass('loaded');
